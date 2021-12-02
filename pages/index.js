87b6c9f4eac9099d4f917679c693c81f7bb2d4d7
Antonio Dottori <aadottori@gmail.com>
2021-12-02 02:48:20 -0300

    Added CV

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Hello, I’m Antonio. I’m a software developer from Brazil.</p>
         <p>You can send me an email at <Link href="mailto:antonio@dottori.in"><a>antonio@dottori.in</a></Link>.</p>
+        <p>Check out my résume <Link href="https://drive.google.com/file/d/1baFXHyGdhQ00zt6VYqNjcL81ID7trJzk/view?usp=sharing"><a>here</a></Link>.</p>
       </section>
     </Layout>
   )
