e2328e899b8dbd1ebc20dd26212e7989831a3b11
Antonio Dottori <aadottori@gmail.com>
2021-12-02 03:49:32 -0300

    Icons

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,24 @@ import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
+
+
 export default function Home() {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
+      <div className="container">
+        <Link href="https://github.com/aadottori"><a><img src="images/icon_github.jpg" width="40" height="40" /></a></Link>
+        <div className="space"></div>
+        <Link href="https://www.linkedin.com/in/antoniodottori/"><a><img src="images/icon_linkedin.jpg" width="40" height="40" /></a></Link>
+        <div className="space"></div>
+        <Link href="mailto:antonio@dottori.in"><a><img src="images/icon_email.jpg" width="40" height="40" /></a></Link>
+
+      </div>
+
       <section className={utilStyles.headingMd}>
         <p>Hello, I’m Antonio. I’m a software developer from Brazil.</p>
         <p>You can send me an email at <Link href="mailto:antonio@dottori.in"><a>antonio@dottori.in</a></Link>.</p>
